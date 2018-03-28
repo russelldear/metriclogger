eaa16114b09d76af3513ebe9777aa177bf72959f
Russell Dear <russell.dear@gmail.com>
2018-03-29 07:11:42 +1300

    Dynamo, broken

---
+++ MetricLogger/Model/Metric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MetricLogger.Model
+{
+    public class MetricLog
+    {
+        public MetricLog()
+        {
+            MetricId = Guid.NewGuid();
+        }
+
+        public Guid MetricId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("value")]
+        public double Value { get; set; }
+
+        [JsonProperty("timestamp")]
+        public DateTime Timestamp { get; set; }
+    }
+}
